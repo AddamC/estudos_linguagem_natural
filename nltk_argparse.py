276c077ba2dbab1d3f3b447a51f1590a7f8e4d4f
AddamC <addamcaue@gmail.com>
2017-07-26 23:21:29 -0300

    basic sentiment analysis implementation

--- nltk_argparse.py
+++ nltk_argparse.py
@@ -1,10 +1,11 @@
 import argparse
-import traceback
 
 import nltk
-from nltk.corpus import mac_morpho, brown
+from nltk.corpus import mac_morpho
 from nltk.tokenize import word_tokenize
-from nltk.sentiment import SentimentAnalyzer, util
+from nltk.stem import PorterStemmer
+import numpy as np
+import re
 
 import json
 
@@ -23,7 +24,11 @@ def process_sentence(d):
         else:
             d_tagged_sent[k] = v
 
-    d['tagged sentence'] = d_tagged_sent
+    d['tagged_sentence'] = d_tagged_sent
+
+    # sentiment analysis
+    check_negative_words(d)
+
     # d['sentence polarity']
 
     string = json.dumps(d, indent=4, ensure_ascii=False, sort_keys=False)
@@ -33,6 +38,37 @@ def process_sentence(d):
     #arquivo.write("\nmac_morpho: " + str (unigram_tagger_mac_morpho.tag (word_tokenize (frase))))
     # file.write(string)
     # file.close()
+
+def check_negative_words(d):
+    ps = PorterStemmer()
+
+    negative_words = ['bad', 'horrible', 'death', 'suffer', 'pain', 'awfull', 'error', 'fail']
+    words = word_tokenize(d.get('sentence'))
+
+    np_negative_words = [ps.stem(w) for w in np.array(negative_words)]
+    np_words = [ps.stem(w) for w in np.array(words)]
+
+    dict_negative_words = {}
+
+    print(np_negative_words)
+    print(np_words)
+
+    negative_counter = 0
+    list_negative = []
+
+    for w in np_words:
+        for neg_w in np_negative_words:
+            if w == neg_w:
+                negative_counter += 1
+                list_negative.append(w)
+
+    dict_negative_words['negative_counter'] = negative_counter
+    dict_negative_words['negative_words'] = list_negative
+
+    negative_perc = negative_counter/len(np_words)
+    dict_negative_words['negative_percentage'] = str(round(negative_perc, 2))
+
+    d['negative_analysis'] = dict_negative_words
 
 def process_portuguese(d):
     mac_morpho_tagged_sents = mac_morpho.tagged_sents () # frases classificadas da mac_morpho
@@ -48,6 +84,10 @@ def process_portuguese(d):
 def process_english(d):
     tagged_sentence = nltk.pos_tag(word_tokenize(d["sentence"]))
     return tagged_sentence
+
+def process_generic(training_set, d):
+
+    return
 
 def Main():
     parser = argparse.ArgumentParser (description="Natural Language Processing")
